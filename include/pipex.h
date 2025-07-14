75e48fda28a7052b425ff50c22c5140026ed6753
Roman Mamzer <mamzer.roman@gmail.com>
2025-07-14 16:52:03 +0300

    minor start

--- include/pipex.h
+++ include/pipex.h
@@ -6,13 +6,20 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:09:57 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/07/11 18:20:43 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/07/14 13:25:21 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-//# include "libft.h"
+//check if used
+# include "libft.h"
 
+# include <unistd.h> // For fork, pipe, dup2, execve, access, close, read, write
+# include <sys/wait.h> // For wait, waitpid
+# include <fcntl.h> // For open, O_RDONLY, O_WRONLY, O_CREAT, O_TRUNC
+# include <stdlib.h> // For exit, malloc, free
+ # include <stdio.h> // For perror
+// # include <string.h> // For strerror (though perror is often enough)
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,46 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:09:21 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/07/11 18:32:02 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/07/14 16:47:10 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main (void)
-
+void error_exit(char *msg)
 {
+ft_putendl_fd(msg, STDERR_FILENO);
+
+exit (EXIT_FAILURE);
+}
+
+
+
+int main (int argc, char **argv, char **envp)
+{
+	int		pipefd[2];
+	pid_t	pid_cmd1;
+	pid_t	pid_cmd2;
+
+	if (argc != 5)
+		error_exit("Usage: ./pipex file1 cmd1 cmd2 file2");
+
+	if (pipe(pipefd) == -1)
+		error_exit("./pipex: pipe broke");
+
+		pid_cmd1 = fork();
+
+	if (pid_cmd1 == -1)
+		error_exit("./pipex: fork 1 failed")
+
 	return (0);
 }
+
+
+
+
+
+/*
+Program structure:
+- receive
+*/
