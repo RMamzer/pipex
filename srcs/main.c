fef682a39dc7965f6517d847a59f3b7d5951d517
Roman Mamzer <mamzer.roman@gmail.com>
2025-07-15 17:43:21 +0300

    added initial start for path search, need to create it before cmd1 execution.

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,27 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:09:21 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/07/15 16:28:29 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/07/15 17:42:24 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+	// should i check for envp ==NULL?
+
 
 void error_exit(char *msg)
 {
 ft_putendl_fd(msg, STDERR_FILENO);
+/*
+NEED TO FREE
+-t_data
+- cmd_ags (ft_split)
+*/
 exit (EXIT_FAILURE);
 }
 
-check_access(char *file, t_data *data)
+void	check_access(char *file, t_data *data)
 {
 	if (access(file, F_OK | R_OK) == -1)
 		error_exit ("Access fail ");
@@ -60,11 +67,18 @@ check_access(char *file, t_data *data)
 		if (!data->cmd_args)
 			// split failed
 			error_exit("split returned NULL");
-		
+		data->cmd_path = check_path(data->argv[2]);
+
 	}
  }
 
-
+void	find_path(t_data *data, char **envp)
+{
+	if (!data->envp ||!*(data->envp))
+		//is this correct
+		error_exit("no path");
+	while (())
+}
 
 int main (int argc, char **argv, char **envp)
 {
@@ -80,6 +94,7 @@ int main (int argc, char **argv, char **envp)
 		error_exit("Calloc broke");
 	data->argv = argv;
 	data->envp = envp;
+	find_path(data, data->envp);
 	if (pipe(data->pipefd) == -1)
 		error_exit("./pipex: pipe broke");
 
