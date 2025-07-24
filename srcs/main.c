42d18edfa2001a8c43689bbc80433fdfe84de797
Roman Mamzer <mamzer.roman@gmail.com>
2025-07-24 13:29:40 +0300

    updated wait, "" "" edgecase

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,11 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:09:21 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/07/23 12:14:31 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/07/24 13:26:08 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-
-// handle -t ?????????????????????????????? YESSSSS
 
  void	*free_arrays(char **splitted)
 {
@@ -25,10 +22,20 @@
 	free(splitted);
 	return (NULL);
 }
-
+void	error_manual(char *msg, t_data *data, int exit_code)
+{
+	ft_putstr_fd("pipex: ", STDERR_FILENO);
+	ft_putendl_fd(msg, STDERR_FILENO);
+		if (data->cmd_args)
+			free_arrays(data->cmd_args);
+	if (data->cmd_path)
+			free_arrays(data->cmd_path);
+	exit (exit_code);
+}
 
 void error_exit(char *msg, t_data *data, int exit_code)
 {
+	ft_putstr_fd("pipex: ", STDERR_FILENO);
 	perror(msg);
 	if (data->cmd_args)
 			free_arrays(data->cmd_args);
@@ -37,13 +44,13 @@ void error_exit(char *msg, t_data *data, int exit_code)
 	exit (exit_code);
 }
 
-void close_fds_and_exit(t_data *data, int opened_file, char *msg, int code)
+void close_fds_and_exit(t_data *data, int opened_file, char *msg, int exit_code)
 {
 	close(data->pipefd[READ_END]);
 	close(data->pipefd[WRITE_END]);
 	if (opened_file >= 0)
 		close (opened_file);
-	error_exit(msg, data, code);
+	error_exit(msg, data, exit_code);
 }
 
 
@@ -67,64 +74,98 @@ char	*path_strjoin(char const *s1, char const *s2)
 	return (joinedstr);
 }
 
-void	execute_cmd(t_data *data, char *path, bool path_in_args)
+// void	execute_cmd(t_data *data, char *path)
+// {
+// 	if (access(path, F_OK| X_OK) == 0)
+// 		{
+// 			execve(path, data->cmd_args, data->envp);
+// 			free(path);
+// 			data->exit_status = CMD_NOT_EXEC;
+// 			return;
+// 		}
+// 	if (errno == EACCES)
+// 		data->exit_status = CMD_NOT_EXEC; //<---exit 126
+// 	else if (data->exit_status != CMD_NOT_EXEC)
+// 		data->exit_status = CMD_NOT_FOUND;//<--- exit 127
+// 	if (errno !=ENOENT)
+// 		error_exit("", data, data->exit_status); //<----- what is this?
+// }
+
+
+
+void	execute_cmd(t_data *data, char *path)
 {
-	if (access(path, F_OK| X_OK) == 0)
+	if (access(path, F_OK)== 0)
+	{
+		if (access (path, X_OK) == 0)
 		{
-			if(execve(path, data->cmd_args, data->envp) == -1)
-			{
-				if (path_in_args == NO_PATH_IN_ARGS)
-					free(path);
-				if (errno == ENOEXEC)
-					data->exit_status = CMD_NOT_EXEC;
-				else
-					data->exit_status = EXIT_FAILURE;
-				return; 
-			}
+			execve(path, data->cmd_args, data->envp);
+			free(path);
+			error_exit("Failed execution", data, CMD_NOT_EXEC);
 		}
-	if (errno == EACCES)
-		data->exit_status = CMD_NOT_EXEC; //<---exit 126
-	else if (data->exit_status != 126 || data->exit_status != EXIT_FAILURE)
-		data->exit_status = CMD_NOT_FOUND; //<--- exit 127
+		free(path);
+		error_exit("Permission denied:", data, CMD_NOT_EXEC);
+	}
+	free (path);
 }
 
-// what uf no envp/cmd_path variables at all?
+void	execute_path_cmd(t_data *data, char *path)
+{
+	if (access(path, F_OK) != 0)
+		error_exit("No such file or directory", data, CMD_NOT_FOUND);
+	if (access(path, X_OK) != 0)
+		error_exit("Permission denied", data, CMD_NOT_EXEC);
+	execve (path, data->cmd_args, data->envp);
+	error_exit("Failed execution", data, CMD_NOT_EXEC);
+}
+
+
 void	check_cmd(t_data *data, char	**cmd_path)
 {
 	char	*joined_path;
 
 	if (!data->cmd_args[0])
-		error_exit("Command not found", data, CMD_NOT_FOUND);
+		error_manual("Command not found", data, CMD_NOT_FOUND);
 	if (ft_strchr(data->cmd_args[0], '/'))
-		execute_cmd(data, data->cmd_args[0], PATH_IN_ARGS);
+		execute_path_cmd(data, data->cmd_args[0]);
+	else if (!cmd_path)
+		error_manual("command not found", data, CMD_NOT_FOUND);
 	else
 	{
 		while(*cmd_path)
 			{
 				joined_path = path_strjoin(*cmd_path, data->cmd_args[0]);
 				if (!joined_path)
-					error_exit("strjoin malloc failed", data, EXIT_FAILURE);
-				execute_cmd(data, joined_path, NO_PATH_IN_ARGS);
-				free (joined_path);
+					error_manual("strjoin malloc failed", data, EXIT_FAILURE);
+				execute_cmd(data, joined_path);
 				cmd_path++;
 			}
 	}
-	error_exit("Cmd was not found", data, data->exit_status);
+	error_manual("command not found", data, CMD_NOT_FOUND);
 }
 
 void wait_children(pid_t pid1, pid_t pid2, t_data *data)
 {
-	int status1;
-	int status2;
+	int	status;
+	pid_t	term_pid;
+	int children_rem;
 
+	children_rem = 2;
 	close(data->pipefd[READ_END]);
 	close(data->pipefd[WRITE_END]);
-	waitpid(pid1, &status1, 0);
-	waitpid(pid2, &status2, 0);
-	if (WIFEXITED(status2))
-		exit(WEXITSTATUS(status2));
-	else
-		exit(EXIT_FAILURE);
+	while (children_rem > 0)
+	{
+		term_pid = waitpid(-1, &status, 0);
+		if (term_pid == -1)
+			error_exit("waitpd failed", data, EXIT_FAILURE);
+		if (term_pid == pid1 || term_pid == pid2)
+		{
+			children_rem--;
+			if (term_pid == pid2 && (WIFEXITED(status)))
+				data->exit_status = WEXITSTATUS(status);
+		}
+	}
+	exit(data->exit_status);
 }
 
 void	find_path(t_data *data, char **envp)
@@ -135,7 +176,7 @@ void	find_path(t_data *data, char **envp)
 			{
 				data->cmd_path = ft_split(*envp + 5 , ':');
 				if (!data->cmd_path)
-					error_exit("failed to split the path to command", data, EXIT_FAILURE);
+					error_manual("failed to split the path to command", data, EXIT_FAILURE);
 				return;
 			}
 		envp++;
@@ -157,9 +198,11 @@ void	child_process_cmd1(t_data *data)
 	close(data->pipefd[WRITE_END]);
 	close(infile_fd);
 	find_path(data, data->envp);
+	if (!data->argv[2][0])
+		error_manual("permission denied", data, CMD_NOT_EXEC);
 	data->cmd_args = ft_split(data->argv[2], ' ');
 	if (!data->cmd_args)
-		error_exit("Failed malloc in splitting arguments", data, EXIT_FAILURE);
+		error_manual("Failed malloc in splitting arguments", data, EXIT_FAILURE);
 	check_cmd(data, data->cmd_path);
 }
 
@@ -178,9 +221,11 @@ void	child_process_cmd2(t_data *data)
 	close(data->pipefd[READ_END]);
 	close(outfile_fd);
 	find_path(data, data->envp);
+	if (!data->argv[3][0])
+		error_manual("permission denied", data, CMD_NOT_EXEC);
 	data->cmd_args = ft_split(data->argv[3], ' ');
 	if (!data->cmd_args)
-		error_exit("Failed malloc in splitting arguments", data, EXIT_FAILURE);
+		error_manual("Failed malloc in splitting arguments", data, EXIT_FAILURE);
 	check_cmd(data,data->cmd_path);
 }
 
@@ -193,7 +238,7 @@ void	init_parent(t_data *data, char **argv, char **envp)
 	data->cmd_path = NULL;
 	data->exit_status = EXIT_FAILURE;
 	if (pipe(data->pipefd) == -1)
-		error_exit("./pipex: pipe broke", data, EXIT_FAILURE);
+		error_exit("Failed pipe", data, EXIT_FAILURE);
 }
 
 
@@ -205,20 +250,24 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc != 5)
 	{
-		ft_printf("Usage: ./pipex file1 cmd1 cmd2 file2");
+		ft_putendl_fd("Usage: ./pipex file1 cmd1 cmd2 file2", STDERR_FILENO);
 		exit (EXIT_FAILURE);
 	}
 	init_parent(&data, argv, envp);
 	pid_cmd1 = fork();
 	if (pid_cmd1 == -1)
-		close_fds_and_exit(&data,NO_OPENED_FILE, "Fork error for cmd1", EXIT_FAILURE);
+		close_fds_and_exit(&data, NO_OPENED_FILE, "Failed fork for cmd1", EXIT_FAILURE);
 	if (pid_cmd1 == 0)
 		child_process_cmd1(&data);
 	pid_cmd2 = fork();
 	if (pid_cmd2 == -1)
-		close_fds_and_exit(&data, NO_OPENED_FILE, "Fork error for cmd2", EXIT_FAILURE);
+		close_fds_and_exit(&data, NO_OPENED_FILE, "Failed fork for cmd2", EXIT_FAILURE);
 	if (pid_cmd2 == 0)
 		child_process_cmd2(&data);
 	wait_children(pid_cmd1, pid_cmd2, &data);
 }
 
+/*
+1. close fd_s
+2.
+*/
